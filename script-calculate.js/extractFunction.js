2e3676d84cdc81b5188ac7c5d19c62e4156b3674
Snu Seam <siamahme766@gmail.com>
2024-10-20 00:31:08 +0600

    commit final

--- script-calculate.js/extractFunction.js
+++ script-calculate.js/extractFunction.js
@@ -1,3 +1,14 @@
 function RhombusBtn(){
-    console.log('gce ghce ');
+    // console.log('gce ghce ');
+    const inputValue = getElementById('input-field');
+    // console.log(inputValue);
+    const inputValue2 = getElementById('inpu-field2');
+    console.log(inputValue2);
+}
+function getElementById(event){
+    const inputField = document.getElementById(event);
+    const inputText = inputField.value;
+    const value = parseFloat(inputText);
+    // console.log(value);
+    return value;
 }
